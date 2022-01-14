a46c3bf84a7aa7ba4121dfd1df78a5429623eda5
EstebanXP <a324249@uach.mx>
2022-01-14 14:35:09 -0700

    0.0 basic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
+//decorator
+//works to change the behavior of the component
 @Component({
   selector: 'app-root',
+  //app-root is the tag to use it on other components
   templateUrl: './app.component.html',
+
+  //specific style sheets
   styleUrls: ['./app.component.css']
 })
+
+//here we put functionality
 export class AppComponent {
-  title = 'store-app';
+  title = 'storeApp';
 }
